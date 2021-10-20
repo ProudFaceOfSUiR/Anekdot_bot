68df8e92a663f258ced3e256126bec054884b8ab
Anatoly <anatolyskier@gmail.com>
2021-10-20 15:22:15 +0300

    almost everything done

--- bot.py
+++ bot.py
@@ -7,11 +7,12 @@ bot = telebot.TeleBot(config.token)
 @bot.message_handler(commands=['start'])
 def welcome(message):
     '''Функция - обработчик стартового запроса, дает юзеру всю инфу'''
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=False)
+    markup.add('1', '2')  # Имена кнопок
     bot.send_message(message.chat.id, "Привет, я расскажу тебе анекдот! Напиши мне одно из следующих ключевых слов"
                                       " и я расскажу тебе анекдот на эту тему:")
     bot.send_message(message.chat.id, "1.Случайный анекдот с чёрным юмором\n2.Случайный добрый анекдот\n"
-                                      "3.Анекдот от балабобы, после номера напиши начало "
-                                      "истории, которую продолжит нейросеть")
+                                      "или просто начни что-то писать, а это продолжит нейросеть",  reply_markup=markup)
 
 peaceful_joke = config.Peacefuljoke()
 bad_joke = config.Badjoke()
@@ -22,13 +23,13 @@ def text(message):
     '''Функция - обрабатывает все приходящие запросы'''
     if message.text == "привет":
         markup = types.ReplyKeyboardMarkup(one_time_keyboard=False)
-        markup.add('1', '2', '3', '4', '5')  # Имена кнопок
+        markup.add('1', '2')  # Имена кнопок
         bot.send_message(message.chat.id, "Привет, я расскажу тебе анекдот!"
                                           " Напиши мне номер одного из следующих ключевых слов,"
                                           " и я расскажу тебе анекдот на эту тему:")
         bot.send_message(message.chat.id, "1.Случайный анекдот с чёрным юмором\n2.Случайный добрый анекдот\n"
-                                          "3.Анекдот от балабобы, после номера напиши начало "
-                                          "истории, которую продолжит нейросеть", reply_markup=markup)
+                                          "или просто начни что-то писать, а это продолжит "
+                                          "нейросеть", reply_markup=markup)
     elif message.text == "3":
         bad_joke.generate()
         bot.send_message(message.chat.id, repr(bad_joke))
